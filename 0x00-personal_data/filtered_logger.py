647a0e09880f3b160c3f2bba11ce5927470ead8c
bashar-karoury <basharalkaroury@hotmail.com>
2024-11-11 20:38:45 +0300

    Update: filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,10 +29,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ initialization method"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ format method to obfuscate PII info before logging"""
         record.msg = filter_datum(self.fields,
                                   self.REDACTION,
                                   record.msg,
