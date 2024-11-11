ff005813e80a01377dfe3b7a6ad7120c024ee6e9
bashar-karoury <basharalkaroury@hotmail.com>
2024-11-11 22:09:16 +0300

    Update: encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -18,4 +18,4 @@ def hash_password(password: str) -> bytes:
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ validate that the provided password matches the hashed password"""
-    return bcrypt.checkpw(password, hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
