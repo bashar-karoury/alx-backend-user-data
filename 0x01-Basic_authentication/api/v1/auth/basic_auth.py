bccb42ee94228078c7c4ccd69e0ae85c390ae2c8
bashar-karoury <basharalkaroury@hotmail.com>
2024-11-12 21:27:21 +0300

    Added files: api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,17 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ BasicAuth class to manage basic authentication mechanism"""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """returns the Base64 part of the Authorization header
+        """
+        # Return None if authorization_header is None
+        # Return None if authorization_header is not a string
+        # Return None if authorization_header doesn’t start by 'Basic '
+        # Otherwise, return the value after Basic (after the space)
+        # You can assume authorization_header contains only one Basic
+        if not authorization_header or type(authorization_header) is not 'str':
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.strip('Basic ')
