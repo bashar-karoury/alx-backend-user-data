9f672ce1609f62fd3bb71dcb7bc959a1ce3be3c8
bashar-karoury <basharalkaroury@hotmail.com>
2024-11-12 21:42:32 +0300

    Added files: api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -24,3 +24,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.strip('Basic ')
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ returns the decoded value of a Base64 string base64_authorization_header
+        """
+        # Return None if base64_authorization_header is None
+        # Return None if base64_authorization_header is not a string
+        # Return None if base64_authorization_header is not a valid Base64
+        # Otherwise, return the decoded value as UTF8 string
+        if not base64_authorization_header or type(base64_authorization_header) is not str:
+            return None
+        try:
+            import base64
+            result = base64.b64decode(base64_authorization_header)
+        except (Exception):
+            return None
+        return result.decode('utf-8')
