5d0404bd5250fa81a9755d643fb7e80ccbb9e83a
bashar-karoury <basharalkaroury@hotmail.com>
2024-11-12 21:45:07 +0300

    Added files: api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -27,13 +27,14 @@ class BasicAuth(Auth):
 
     def decode_base64_authorization_header(
             self, base64_authorization_header: str) -> str:
-        """ returns the decoded value of a Base64 string base64_authorization_header
+        """ returns the decoded value of parameter str
         """
         # Return None if base64_authorization_header is None
         # Return None if base64_authorization_header is not a string
         # Return None if base64_authorization_header is not a valid Base64
         # Otherwise, return the decoded value as UTF8 string
-        if not base64_authorization_header or type(base64_authorization_header) is not str:
+        if (not base64_authorization_header or
+                type(base64_authorization_header) is not str):
             return None
         try:
             import base64
