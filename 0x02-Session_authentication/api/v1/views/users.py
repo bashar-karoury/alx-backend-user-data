588cea1f2f8cd148f9beb597d1b9848086a0fc3a
bashar-karoury <basharalkaroury@hotmail.com>
2024-11-15 16:29:16 +0300

    Added files: api/v1/views/users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -33,7 +33,10 @@ def view_one_user(user_id: str = None) -> str:
         else:
             return jsonify(request.current_user.to_json())
     else:
-        abort(404)
+        user = User.get(user_id)
+        if user is None:
+            abort(404)
+        return jsonify(user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
