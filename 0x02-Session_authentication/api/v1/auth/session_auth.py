24ff08500fe08122e623f70fec7480102a3a9879
bashar-karoury <basharalkaroury@hotmail.com>
2024-11-15 11:37:04 +0300

    Added files: api/v1/auth/session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,3 +19,16 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
+
+# Create an instance method def user_id_for_session_id(self, session_id: str = None) -> str: that returns a User ID based on a Session ID:
+
+# Return None if session_id is None
+# Return None if session_id is not a string
+# Return the value (the User ID) for the key session_id in the dictionary user_id_by_session_id.
+# You must use .get() built-in for accessing in a dictionary a value based on key
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ retrives user_id from session_id"""
+        if session_id is None or type(session_id) != str:
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
