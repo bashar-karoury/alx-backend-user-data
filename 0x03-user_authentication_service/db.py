c45bf94320a51774bb488f0bc2facc8eec8efc6f
bashar_karoury <basharalkaroury@hotmail.com>
2024-11-21 11:13:07 +0300

    Updated files: db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -67,6 +67,7 @@ class DB:
                     if not hasattr(User, k):
                         raise ValueError('value error')
                     user.__setattr__(k, v)
+                self._session.add(user)
                 self._session.commit()
             else:
                 raise ValueError
