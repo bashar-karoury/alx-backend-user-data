5eba695346487b6e30160afbeb1cc88e9a0721cf
bashar_karoury <basharalkaroury@hotmail.com>
2024-11-21 12:03:05 +0300

    Updated files: app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,5 +10,10 @@ def simple_route():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST', 'GET'])
+def register_user():
+    return jsonify({"message": "Bienvenue"})
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
