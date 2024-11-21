70a48703609d0078bf720363fa7e91b42ae0858e
bashar_karoury <basharalkaroury@hotmail.com>
2024-11-21 13:21:13 +0300

    Updated files: app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,27 +2,31 @@
 """ Flask App """
 from flask import Flask, jsonify, request
 from auth import Auth
-
+from flask_cors import CORS
+from typing import Any
 
 AUTH = Auth()
 app = Flask('main')
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.route('/')
-def simple_route():
+def simple_route() -> None:
     return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def register_user_route():
-    data = request.get_json()
-    email = data.get('email')
-    password = data.get('password')
-    if not email or not password:
+def register_user_route() -> tuple[Any, int]:
+    form_email = request.form.get('email')
+    form_pass = request.form.get('password')
+    # data = request.get_json()
+    # email = data.get('email')
+    # password = data.get('password')
+    if not form_email or not form_pass:
         return jsonify({"message": "args provided are incorrect"}), 400
     try:
-        user = AUTH.register_user(email=email, password=password)
-        return jsonify({"email": email, "message": "user created"})
+        user = AUTH.register_user(email=form_email, password=form_pass)
+        return jsonify({"email": form_email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
