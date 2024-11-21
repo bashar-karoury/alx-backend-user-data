a37960806baceee46a6c574cf2b1b55b5a11ee4d
bashar_karoury <basharalkaroury@hotmail.com>
2024-11-21 18:18:19 +0300

    Updated files: app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -90,5 +90,20 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """ update password route"""
+    f_email = request.form.get('email')
+    form_reset_token = request.form.get('reset_token')
+    form_new_password = request.form.get('new_password')
+    if not (f_email and form_reset_token and form_new_password):
+        abort(403)
+    try:
+        AUTH.update_password(form_reset_token, form_new_password)
+        return jsonify({"email": f_email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
