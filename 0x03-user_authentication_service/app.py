a5c5f109ae0948b07fc23ea0ec87686f61036ac1
bashar_karoury <basharalkaroury@hotmail.com>
2024-11-21 16:20:20 +0300

    Updated files: app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """ Flask App """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 from flask_cors import CORS
 from typing import Any
 
 AUTH = Auth()
 app = Flask('main')
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/')
@@ -31,6 +31,23 @@ def users() -> tuple[Any, int]:
         return jsonify({"email": form_email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ login route"""
+
+    form_email = request.form.get('email')
+    form_pass = request.form.get('password')
+    if not form_email or not form_pass:
+        return jsonify({"message": "args provided are incorrect"}), 400
+    if not AUTH.valid_login(email=form_email, password=form_pass):
+        abort(401)
+    sess_id = AUTH.create_session(form_email)
+    resp = make_response(
+        jsonify({"email": "<user email>", "message": "logged in"}), 200)
+    resp.set_cookie('session_id', sess_id)
+    return resp
 
 
 if __name__ == "__main__":
