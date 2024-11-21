b2e9f81805663cbb90d32a60d540c854ca2df6b4
bashar_karoury <basharalkaroury@hotmail.com>
2024-11-21 14:03:49 +0300

    Updated files: auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,3 +31,12 @@ class Auth:
         except NoResultFound:
             hashed_pass = _hash_password(password)
             return self._db.add_user(email=email, hashed_password=hashed_pass)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ validate login info"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
+        except NoResultFound:
+            return False
