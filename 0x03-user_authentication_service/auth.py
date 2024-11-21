8b183b5a51bcec2022a1a94c87521ea413b2a7ff
bashar_karoury <basharalkaroury@hotmail.com>
2024-11-21 17:11:06 +0300

    Updated files: auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -82,7 +82,7 @@ class Auth:
     def get_reset_password_token(self, email: str) -> str:
         """ returns resest password token"""
         try:
-            user = self._db.find_user_by(eamil=email)
+            user = self._db.find_user_by(email=email)
             reset_token = str(uuid.uuid4())
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
