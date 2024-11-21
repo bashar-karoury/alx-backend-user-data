69af32fbc9b56aeda379b219663077a3c7b24ae8
bashar_karoury <basharalkaroury@hotmail.com>
2024-11-21 12:05:15 +0300

    Updated files: app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """ Flask App """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask('main')
 
 
@@ -9,9 +12,19 @@ app = Flask('main')
 def simple_route():
     return jsonify({"message": "Bienvenue"})
 
+# The end-point should expect two form data fields: "email" and "password". If the user does not exist, the end-point should register it and respond with the following JSON payload:
+
+# {"email": "<registered email>", "message": "user created"}
+# If the user is already registered, catch the exception and return a JSON payload of the form
+
+# {"message": "email already registered"}
+# and return a 400 status code
+
 
 @app.route('/users', methods=['POST', 'GET'])
 def register_user():
+    print(request.json())
+
     return jsonify({"message": "Bienvenue"})
 
 
